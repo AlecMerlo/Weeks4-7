4ed11879ea1c433f6498cc2d396fe814a3ae1322
Alec Merlo <merloa@shernet.sheridancollege.ca>
2025-01-28 16:50:47 -0500

    made things turn on and off

---
+++ Assets/Scripts/On and Off/EnableDisable.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDisable : MonoBehaviour
+{
+    public GameObject go;
+    public SpriteRenderer sr;
+    public EnableDisable script;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            sr.enabled = false;
+            go.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            sr.enabled = true;
+            go.SetActive(true);
+        }
+        // 'go.activeInHierarchy' -- to check if it is enabled or disabled
+    }
+}
